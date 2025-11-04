c621fe8c54a3fa16c9efcce987eb71469b906f8b
Jon Kunkee <jonathan.kunkee@gmail.com>
2025-11-03 16:05:35 -0800

    Comments, data production state machine, refresh, half-dev scenario

--- firmware/badge/apps/atmosdata.py
+++ firmware/badge/apps/atmosdata.py
@@ -27,54 +27,64 @@ class AtmosphereData(BaseApp):
     def __init__(self, name: str, badge):
         super().__init__(name, badge)
 
+        # device constants
         self.sensor_refresh_interval_ms = 5000
         scd30_address = 0x61
         sps30_address = 0x69
 
+        # main app timing info
         self.foreground_sleep_ms = 10
         self.background_sleep_ms = self.sensor_refresh_interval_ms
 
+        # See what's out there and set it up
+        # n.b. this is usually naive and ignores i2c spec discovery mechanics
         i2c_scan_result = self.badge.sao_i2c.scan()
 
         if scd30_address in i2c_scan_result:
             self.scd30 = SCD30(self.badge.sao_i2c, scd30_address) # leave internal sleep at default 1000us
             self.scd30.set_measurement_interval(int(self.sensor_refresh_interval_ms/1000))
-            self.producing_data = True
         else:
             self.scd30 = None
-            self.producing_data = False
 
         if sps30_address in i2c_scan_result:
             self.sps30 = SPS30(self.badge.sao_i2c, sps30_address)
             self.sps30.start_measurement()
-            #self.sps30 = None
         else:
             self.sps30 = None
 
-        self.co2_measurement = [-1, -1, -1]
+        # If we have a sensor, act as a data producer; otherwise, listen on LoRa
+        self.producing_data = self.scd30 != None or self.sps30 != None
+
+        # To decouple the timing of LoRa broadcasts, UI updates, and sensor reads,
+        # each sensor reading is cached in these variables.
+        # TODO: don't Fill out dummy data to avoid crashes when the sequencing isn't right, just get it right
+        self.co2_measurement = [-1.0, -1.0, -1.0]
         self.particle_measurement = []
         for idx in range(0,4+5+2):
             self.particle_measurement.append(["",-1.0]) # incomplete dummy data
+
+        # Data freshness indicator
         self.screen_has_latest_data = True
+        # LoRa rate limiter (minimum broadcast interval is sensor_refresh_interval_ms)
         self.last_transmission = 0
 
+        # UI object tracking (TODO: lean heavier on LVGL and micropython to avoid this)
         self.current_line_labels = []
 
     def start(self):
         super().start()
-        register_receiver(ATMOS_PROTOCOL, self.receive_message)
+        if not self.producing_data:
+            register_receiver(ATMOS_PROTOCOL, self.receive_message)
 
     def receive_message(self, message: NetworkFrame):
         """Handle incoming messages."""
-        print(f"atmos received message {message.payload}")
-        # TODO do this check for register_receiver instead (this is easier to debug)
-        if not self.producing_data and message.port == ATMOS_PROTOCOL.port and message.payload[0] == ATMOS_VERSION:
+        print(f"atmos received message {message.payload}") # A bit chatty, innit
+        if message.port == ATMOS_PROTOCOL.port and message.payload[0] == ATMOS_VERSION:
             self.co2_measurement = message.payload[1:3]
             self.particle_measurement = message.payload[4:8]
             self.screen_has_latest_data = False
 
     def poll_data(self):
-        # safety
         if not self.producing_data:
             return
         # This scd30 driver isn't very resilient to the device falling off the bus sometimes,
@@ -92,6 +102,7 @@ class AtmosphereData(BaseApp):
                 transmit_new_data = True
                 self.particle_measurement = self.sps30.read_measurement()
                 print(f"part: {self.particle_measurement}")
+            # Some sensors update frequently, so use holdoff to avoid spamming LoRa
             if transmit_new_data and (now - self.last_transmission) > self.sensor_refresh_interval_ms:
                 tx_msg = NetworkFrame().set_fields(protocol=ATMOS_PROTOCOL,
                                                 destination=BROADCAST_ADDRESS,
@@ -112,26 +123,41 @@ class AtmosphereData(BaseApp):
         except:
             print("scd30 read failure")
 
-    def compose_lines(self) -> list[str]:
-        l = []
-        l.append(f"{self.co2_measurement[0]:.0f} ppm CO2")
-        l.append(f"{self.co2_measurement[1]:.2f} deg C ({(self.co2_measurement[1] * 9 / 5) + 32:.0f} deg F)")
-        l.append(f"{self.co2_measurement[2]}% rh")
-        l.append(f"{self.particle_measurement[4][1]} {self.particle_measurement[4][0]} particles/cm^3")
-        l.append(f"{self.particle_measurement[5][1]} {self.particle_measurement[5][0]} particles/cm^3")
-        l.append(f"{self.particle_measurement[6][1]} {self.particle_measurement[6][0]} particles/cm^3")
-        l.append(f"{self.particle_measurement[7][1]} {self.particle_measurement[7][0]} particles/cm^3")
-        l.append(f"{self.particle_measurement[8][1]} {self.particle_measurement[8][0]} particles/cm^3")
-        return l
-
     def refresh_labels(self) -> None:
+        if self.screen_has_latest_data:
+            return
+        else:
+            self.screen_has_latest_data = True
+
+        text_to_display = []
+
+        if self.producing_data and not self.scd30:
+            text_to_display.append("SCD30 CO2 sensor not present")
+            text_to_display.append("")
+            text_to_display.append("")
+        else:
+            text_to_display.append(f"{self.co2_measurement[0]:.0f} ppm CO2")
+            text_to_display.append(f"{self.co2_measurement[1]:.2f} deg C ({(self.co2_measurement[1] * 9 / 5) + 32:.0f} deg F)")
+            text_to_display.append(f"{self.co2_measurement[2]}% rh")
+
+        if self.producing_data and not self.sps30:
+            text_to_display.append("SCD30 CO2 sensor not present")
+            text_to_display.append("")
+            text_to_display.append("")
+        else:
+            text_to_display.append(f"{self.particle_measurement[4][1]} {self.particle_measurement[4][0]} particles/cm^3")
+            text_to_display.append(f"{self.particle_measurement[5][1]} {self.particle_measurement[5][0]} particles/cm^3")
+            text_to_display.append(f"{self.particle_measurement[6][1]} {self.particle_measurement[6][0]} particles/cm^3")
+            text_to_display.append(f"{self.particle_measurement[7][1]} {self.particle_measurement[7][0]} particles/cm^3")
+            text_to_display.append(f"{self.particle_measurement[8][1]} {self.particle_measurement[8][0]} particles/cm^3")
+
         # I should be able to get LVGL to do vertical stacking for me
         # Many thanks to hwmon for showing how to do some of this
         y_pos = 18
         for label in self.current_line_labels:
             label.delete()
         self.current_line_labels = []
-        text_to_display = self.compose_lines()
+
         for text in text_to_display:
             label = lvgl.label(self.badge.display.screen)
             self.current_line_labels.append(label)
@@ -140,14 +166,8 @@ class AtmosphereData(BaseApp):
             y_pos += 13
 
     def run_foreground(self):
-        if self.producing_data:
-            self.poll_data()
-        else:
-            pass # Wait for lora packets
-
-        if not self.screen_has_latest_data:
-            self.screen_has_latest_data = True
-            self.refresh_labels()
+        self.poll_data() # Does nothing if no sensors present
+        self.refresh_labels() # Does nothing if data is not new
 
         if self.badge.keyboard.f1():
             pass
@@ -164,10 +184,7 @@ class AtmosphereData(BaseApp):
 
     def run_background(self):
         super().run_background()
-        if self.producing_data:
-            self.poll_data()
-        else:
-            pass # Wait for lora packets
+        self.poll_data() # Does nothing if no sensors present
 
     def switch_to_foreground(self):
         super().switch_to_foreground()
@@ -183,6 +200,7 @@ class AtmosphereData(BaseApp):
         else:
             self.p.infobar_right.set_text(f"Polling sensors every ~{int(self.sensor_refresh_interval_ms/1000)}s")
         self.screen_has_latest_data = False
+        self.refresh_labels()
 
     def switch_to_background(self):
         # TODO: If the LVGL objects are properly parented, this loop may not be necessary.
